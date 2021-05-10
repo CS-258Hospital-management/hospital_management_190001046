3106b0eb0a8fb284b7f19a52fd2994131fac88c4
Bhuvana-13 <72562855+Bhuvana-13@users.noreply.github.com>
2021-05-10 22:50:57 +0530

    Update test_forms.py

--- test_forms.py
+++ test_forms.py
@@ -1,36 +1,7 @@
 from django.test import SimpleTestCase
-from web_app.forms import SugarForm,bpForm,doctorprofileForm,presForm,patientprofileForm
+from web_app.forms import doctorprofileForm,presForm,patientprofileForm
 
 class TestForms(SimpleTestCase):
-
-    def test_sugar_form_valid_data(self):
-        form = SugarForm(data={
-            'userid' : 1,
-            'testid' : 2
-        })
-        
-        self.assertIsNone('userid')
-
-
-    def test_sugar_form_no_data(self):
-            form = SugarForm(data={})
-            self.assertFalse(form.is_valid())
-            self.assertEquals(len(form.errors),2)
-
-    def test_bp_form_valid_data(self):
-        form = bpForm(data={
-            'uid' : 1,
-            'tid' : 2
-        })
-        self.assertTrue(form.is_valid())
-
-
-    def test_bp_form_no_data(self):
-            form = bpForm(data={})
-
-            self.assertFalse(form.is_valid())
-            self.assertEquals(len(form.errors),2)        
-
     
     def test_pres_form_valid_data(self):
         form = presForm(data={
