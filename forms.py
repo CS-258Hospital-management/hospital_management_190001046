f2e2771ec9a68fa408c035f9a6eb1bcdeaf042f1
Bhuvana-13 <72562855+Bhuvana-13@users.noreply.github.com>
2021-05-10 22:50:12 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,13 +1,5 @@
 from django import forms
 
-class SugarForm(forms.Form):
-    userid = forms.IntegerField()
-    testid = forms.IntegerField()
-
-class bpForm(forms.Form):
-    uid = forms.IntegerField()
-    tid = forms.IntegerField()
-
 class presForm(forms.Form):
     uid = forms.IntegerField()
    
